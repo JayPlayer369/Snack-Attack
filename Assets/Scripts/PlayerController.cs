dbbe04a19c1f858c72ee8db6ba4ed29706478a51
Ahacker1208 <97680603+Ahacker1208@users.noreply.github.com>
2023-11-03 13:22:48 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput= Input.GetAxis("Vertical");
+
+        //moves the vehicle forward
+        //Time.deltaTime helps to keep track the speed and time between all of the different frames
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right * turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public float spawnRangeX = 20;
+    public float spawnPosZ = 20;
+    public GameObject[] animalPrefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+         InvokeRepeating("SpawnRandomAnimal", 2, 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-20, 22), 50, Random.Range(-14, 11));
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+}
+}
+    
